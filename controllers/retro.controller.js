6251ca6e57c28e63b377a8543d964437949d3ac1
Phelipe Rocha <phelipeafonso@gmail.com>
2018-10-12 18:50:20 -0300

    test(RetroController): SHOULD NOT return for a nonexistent retro

--- controllers/retro.controller.js
+++ controllers/retro.controller.js
@@ -1,29 +1,36 @@
+const createError = require('http-errors');
+
 const retroMock = require('../mocks/retro.mock');
 const listsMock = require('../mocks/list.mock');
 const cardsMock = require('../mocks/card.mock');
 const usersMock = require('../mocks/user.mock');
 
-exports.get = function(req, res) {
+exports.get = function(req, res, next) {
     let obj = retroMock.find(retro => retro.id === parseInt(req.params.id));
-    return res.status(200).send(obj);
+    if (obj) res.status(200).send(obj);
+    else return next(createError(404));
 };
 
-exports.getByPin = function(req, res) {
+exports.getByPin = function(req, res, next) {
     let obj = retroMock.find(retro => retro.pin == req.params.pin);
-    return res.status(200).send(obj);
+    if (obj) res.status(200).send(obj);
+    else return next(createError(404));
 };
 
-exports.getLists = function(req, res) {
+exports.getLists = function(req, res, next) {
     let obj = listsMock.map(list => { list.retroid = req.params.id; return list; });
-    return res.status(200).send(obj);
+    if (retroMock[req.params.id]) res.status(200).send(obj);
+    else return next(createError(404));
 };
 
-exports.getCards = function(req, res) {
+exports.getCards = function(req, res, next) {
     let obj = cardsMock.map(card => { card.retroid = req.params.id; return card; });
-    return res.status(200).send(obj);
+    if (retroMock[req.params.id]) res.status(200).send(obj);
+    else return next(createError(404));
 };
 
-exports.getFacilitator = function(req, res) {
+exports.getFacilitator = function(req, res, next) {
     let obj = usersMock[0];
-    return res.status(200).send(obj);
+    if (retroMock[req.params.id]) res.status(200).send(obj);
+    else return next(createError(404));
 };
--- test/retro.controller.spec.js
+++ test/retro.controller.spec.js
@@ -15,7 +15,7 @@ chai.use(chaiHttp);
 
 describe('RetroController', function() {
     describe('GET', function() {
-        it('should return a retro object by id', function() {
+        it('SHOULD return a retro object by id', function() {
             chai.request(server)
                 .get('/retro/1')
                 .end((err, res) => {
@@ -24,7 +24,17 @@ describe('RetroController', function() {
                 });
         });
 
-        it('should return a retro object by PIN code', function() {
+        it('SHOULD NOT return a retro for a nonexistent retro id', function() {
+            chai.request(server)
+                .get('/retro/5')
+                .end((err, res) => {
+                    expect(res.status).to.be.equal(404);
+                    expect(res.body.errors).to.have.lengthOf(1);
+                    expect(res.body.errors[0].message).to.be.equal('Not Found');
+                });
+        });
+
+        it('SHOULD return a retro object by PIN code', function() {
             chai.request(server)
                 .get('/retro/pincode/9876543')
                 .end((err, res) => {
@@ -33,7 +43,17 @@ describe('RetroController', function() {
                 });
         });
 
-        it('should return a array of list from a retro', function() {
+        it('SHOULD NOT return a retro for a nonexistent PIN code', function() {
+            chai.request(server)
+                .get('/retro/pincode/5843214')
+                .end((err, res) => {
+                    expect(res.status).to.be.equal(404);
+                    expect(res.body.errors).to.have.lengthOf(1);
+                    expect(res.body.errors[0].message).to.be.equal('Not Found');
+                });
+        });
+
+        it('SHOULD return a array of list from a retro', function() {
             chai.request(server)
                 .get('/retro/1/lists')
                 .end((err, res) => {
@@ -43,7 +63,17 @@ describe('RetroController', function() {
                 });
         });
 
-        it('should return a array of cards from a retro', function() {
+        it('SHOULD NOT return an array of list for a nonexistent retro', function() {
+            chai.request(server)
+                .get('/retro/5/lists')
+                .end((err, res) => {
+                    expect(res.status).to.be.equal(404);
+                    expect(res.body.errors).to.have.lengthOf(1);
+                    expect(res.body.errors[0].message).to.be.equal('Not Found');
+                });
+        });
+
+        it('SHOULD return a array of cards from a retro', function() {
             chai.request(server)
                 .get('/retro/1/cards')
                 .end((err, res) => {
@@ -53,12 +83,32 @@ describe('RetroController', function() {
                 });
         });
 
-        it('should return the facilitador of the retro', function() {
+        it('SHOULD NOT return an array of cards for a nonexistent retro', function() {
+            chai.request(server)
+                .get('/retro/5/cards')
+                .end((err, res) => {
+                    expect(res.status).to.be.equal(404);
+                    expect(res.body.errors).to.have.lengthOf(1);
+                    expect(res.body.errors[0].message).to.be.equal('Not Found');
+                });
+        });
+
+        it('SHOULD return the facilitator of the retro', function() {
             chai.request(server)
                 .get('/retro/1/user')
                 .end((err, res) => {
                     expect(res.status).to.be.equal(200);
                     expect(res.body).to.deep.equal(usersMock[0]);
+                });
+        });
+
+        it('SHOULD NOT return the facilitator for a nonexistent retro', function() {
+            chai.request(server)
+                .get('/retro/5/user')
+                .end((err, res) => {
+                    expect(res.status).to.be.equal(404);
+                    expect(res.body.errors).to.have.lengthOf(1);
+                    expect(res.body.errors[0].message).to.be.equal('Not Found');
                 });
         });
     });
