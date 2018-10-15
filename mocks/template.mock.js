2d9eef00b6f42ac54ee5dbc78594f1bd7ccaf3bd
Phelipe Rocha <phelipeafonso@gmail.com>
2018-10-15 13:24:13 -0300

    chore: TemplateController | GET | getAll | Mock | Test

--- mocks/template.mock.js
+++ mocks/template.mock.js
@@ -1,15 +1,15 @@
 module.exports = [
     {
         'id': 1,
-        'title': 'Template 1',
-        'image': null,
+        'name': 'Template 1',
+        'image': 'http://localhost:4200/assets/images/hopes_concern.png',
         'structure': {},
         'status': true,
     },
     {
         'id': 2,
-        'title': 'Template 1',
-        'image': null,
+        'name': 'Template 1',
+        'image': 'http://localhost:4200/assets/images/nice_ok.png',
         'structure': {},
         'status': true,
     },
