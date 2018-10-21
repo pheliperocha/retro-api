7e42b378c53ac6cf33e30098754fa389541f5f71
Phelipe Rocha <phelipeafonso@gmail.com>
2018-10-21 11:10:27 -0300

    feat: Update database to camelCase and rename action table to annotation

--- db/schema/creates.sql
+++ db/schema/creates.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `lastname` VARCHAR(70) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
   `image` VARCHAR(2083) NULL,
-  `linkedinid` VARCHAR(255) NULL,
+  `linkedinId` VARCHAR(255) NULL,
   `status` TINYINT NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`));
 
@@ -22,11 +22,11 @@ CREATE TABLE IF NOT EXISTS `retro` (
   `image` VARCHAR(2083) NULL,
   `pin` INT(7) NULL,
   `status` TINYINT NULL DEFAULT 1,
-  `userid` INT(11) NOT NULL,
+  `userId` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `userid_idx` (`userid` ASC),
-  CONSTRAINT `userid`
-    FOREIGN KEY (`userid`)
+  INDEX `userId_idx` (`userId` ASC),
+  CONSTRAINT `userId`
+    FOREIGN KEY (`userId`)
     REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
@@ -38,13 +38,13 @@ CREATE TABLE IF NOT EXISTS `retro` (
 CREATE TABLE IF NOT EXISTS `list` (
   `id` INT(11) NOT NULL,
   `title` VARCHAR(70) NOT NULL,
-  `retroid` INT(11) NOT NULL,
+  `retroId` INT(11) NOT NULL,
   `status` TINYINT NOT NULL DEFAULT 1,
   `position` INT(2) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `retroid_idx` (`retroid` ASC),
-  CONSTRAINT `retroid`
-    FOREIGN KEY (`retroid`)
+  INDEX `retroId_idx` (`retroId` ASC),
+  CONSTRAINT `retroId`
+    FOREIGN KEY (`retroId`)
     REFERENCES `retro` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
@@ -56,27 +56,27 @@ CREATE TABLE IF NOT EXISTS `list` (
 CREATE TABLE IF NOT EXISTS `card` (
   `id` INT(11) NOT NULL,
   `description` VARCHAR(1024) NOT NULL,
-  `listid` INT(11) NOT NULL,
-  `userid` INT(11) NOT NULL,
-  `retroid` INT(11) NOT NULL,
+  `listId` INT(11) NOT NULL,
+  `userId` INT(11) NOT NULL,
+  `retroId` INT(11) NOT NULL,
   `status` TINYINT NOT NULL DEFAULT 1,
   `position` INT(2) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `retroid_idx` (`retroid` ASC),
-  INDEX `listid_idx` (`listid` ASC),
-  INDEX `userid_idx` (`userid` ASC),
-  CONSTRAINT `retroid`
-    FOREIGN KEY (`retroid`)
+  INDEX `retroId_idx` (`retroId` ASC),
+  INDEX `listId_idx` (`listId` ASC),
+  INDEX `userId_idx` (`userId` ASC),
+  CONSTRAINT `retroId`
+    FOREIGN KEY (`retroId`)
     REFERENCES `retro` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `listid`
-    FOREIGN KEY (`listid`)
+  CONSTRAINT `listId`
+    FOREIGN KEY (`listId`)
     REFERENCES `list` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `userid`
-    FOREIGN KEY (`userid`)
+  CONSTRAINT `userId`
+    FOREIGN KEY (`userId`)
     REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
@@ -87,19 +87,19 @@ CREATE TABLE IF NOT EXISTS `card` (
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `member` (
   `id` INT(11) NOT NULL,
-  `retroid` INT(11) NOT NULL,
-  `userid` INT(11) NOT NULL,
+  `retroId` INT(11) NOT NULL,
+  `userId` INT(11) NOT NULL,
   `status` TINYINT NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`),
-  INDEX `retroid_idx` (`retroid` ASC),
-  INDEX `userid_idx` (`userid` ASC),
-  CONSTRAINT `retroid`
-    FOREIGN KEY (`retroid`)
+  INDEX `retroId_idx` (`retroId` ASC),
+  INDEX `userId_idx` (`userId` ASC),
+  CONSTRAINT `retroId`
+    FOREIGN KEY (`retroId`)
     REFERENCES `retro` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `userid`
-    FOREIGN KEY (`userid`)
+  CONSTRAINT `userId`
+    FOREIGN KEY (`userId`)
     REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
@@ -110,19 +110,19 @@ CREATE TABLE IF NOT EXISTS `member` (
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `cardvote` (
   `id` INT(11) NOT NULL,
-  `cardid` INT(11) NOT NULL,
-  `userid` INT(11) NOT NULL,
+  `cardId` INT(11) NOT NULL,
+  `userId` INT(11) NOT NULL,
   `status` INT(11) NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`),
-  INDEX `cardid_idx` (`cardid` ASC),
-  INDEX `userid_idx` (`userid` ASC),
-  CONSTRAINT `cardid`
-    FOREIGN KEY (`cardid`)
+  INDEX `cardId_idx` (`cardId` ASC),
+  INDEX `userId_idx` (`userId` ASC),
+  CONSTRAINT `cardId`
+    FOREIGN KEY (`cardId`)
     REFERENCES `card` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `userid`
-    FOREIGN KEY (`userid`)
+  CONSTRAINT `userId`
+    FOREIGN KEY (`userId`)
     REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
@@ -134,12 +134,12 @@ CREATE TABLE IF NOT EXISTS `cardvote` (
 CREATE TABLE IF NOT EXISTS `accesstoken` (
   `id` VARCHAR(64) NOT NULL,
   `ttl` INT NOT NULL DEFAULT 1209600,
-  `userid` INT(11) NOT NULL,
+  `userId` INT(11) NOT NULL,
   `created` TIMESTAMP NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `userid_idx` (`userid` ASC),
-  CONSTRAINT `userid`
-    FOREIGN KEY (`userid`)
+  INDEX `userId_idx` (`userId` ASC),
+  CONSTRAINT `userId`
+    FOREIGN KEY (`userId`)
     REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
@@ -150,61 +150,61 @@ CREATE TABLE IF NOT EXISTS `accesstoken` (
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `template` (
   `id` INT(11) NOT NULL,
-  `userid` INT(11) NOT NULL,
+  `userId` INT(11) NOT NULL,
   `title` VARCHAR(70) NOT NULL,
   `image` VARCHAR(2083) NOT NULL,
   `structure` TEXT NOT NULL,
   `status` TINYINT NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`),
-  INDEX `userid_idx` (`userid` ASC),
-  CONSTRAINT `userid`
-    FOREIGN KEY (`userid`)
+  INDEX `userId_idx` (`userId` ASC),
+  CONSTRAINT `userId`
+    FOREIGN KEY (`userId`)
     REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
 
 -- -----------------------------------------------------
--- Table `action`
+-- Table `annotation`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `action` (
+CREATE TABLE IF NOT EXISTS `annotation` (
   `id` INT(11) NOT NULL,
-  `cardid` INT(11) NOT NULL,
-  `userid` INT(11) NOT NULL,
+  `cardId` INT(11) NOT NULL,
+  `userId` INT(11) NOT NULL,
   `description` TEXT NOT NULL,
   `status` TINYINT NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`),
-  INDEX `cardid_idx` (`cardid` ASC),
-  INDEX `userid_idx` (`userid` ASC),
-  CONSTRAINT `cardid`
-    FOREIGN KEY (`cardid`)
+  INDEX `cardId_idx` (`cardId` ASC),
+  INDEX `userId_idx` (`userId` ASC),
+  CONSTRAINT `cardId`
+    FOREIGN KEY (`cardId`)
     REFERENCES `card` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `userid`
-    FOREIGN KEY (`userid`)
+  CONSTRAINT `userId`
+    FOREIGN KEY (`userId`)
     REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
 
 -- -----------------------------------------------------
--- Table `actionresponsible`
+-- Table `annotationResponsible`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `actionresponsible` (
+CREATE TABLE IF NOT EXISTS `annotationResponsible` (
   `id` INT NOT NULL,
-  `actionid` INT(11) NOT NULL,
-  `userid` INT(11) NOT NULL,
+  `annotationId` INT(11) NOT NULL,
+  `userId` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `actionid_idx` (`actionid` ASC),
-  INDEX `userid_idx` (`userid` ASC),
-  CONSTRAINT `actionid`
-    FOREIGN KEY (`actionid`)
-    REFERENCES `action` (`id`)
+  INDEX `annotationId_idx` (`annotationId` ASC),
+  INDEX `userId_idx` (`userId` ASC),
+  CONSTRAINT `annotationId`
+    FOREIGN KEY (`annotationId`)
+    REFERENCES `annotation` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `userid`
-    FOREIGN KEY (`userid`)
+  CONSTRAINT `userId`
+    FOREIGN KEY (`userId`)
     REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
